701a7ac413f9d3166e29b6f60356b15c5bbb3b4e
Luke Shay <shay.luke17@gmail.com>
2022-05-01 20:17:06 -0500

    fly deploy

--- makefile
+++ makefile
@@ -1,7 +1,14 @@
 DOCKER ?= $(shell which docker)
-
+MIX ?= $(shell which mix)
 
 .PHONY: dcp-up
 dcp-up:
 	@$(DOCKER) compose up -d
-mix
+
+.PHONY: release
+release:
+	@$(MIX) deps.get --only prod
+	@MIX_ENV=prod $(MIX) compile
+	@MIX_ENV=prod $(MIX) assets.deploy
+	@$(MIX) phx.gen.release
+	@MIX_ENV=prod $(MIX) release
--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule HomeInventory.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["cmd --cd assets node scripts/build.js --deploy", "phx.digest"]
+      "assets.deploy": ["cmd --cd assets npm run deploy --deploy", "phx.digest"]
     ]
   end
 end
